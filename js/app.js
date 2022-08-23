db4986c1a9a840535409dd04540dfbc2afad3af1
unknown <dikun.yulia@gmail.com>
2022-08-23 21:11:56 +0300

    create classes and game logic

--- js/app.js
+++ js/app.js
@@ -1,40 +1,111 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+const cell = {
+	width: 100,
+	height: 85
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+const gameField = {
+	top: 0,
+	right: cell.width * 5,
+	bottom: cell.height * 6,
+	left: 0
 };
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+const playerStart = {
+	x: 202,
+	y: 400
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+const enemySpeed = {
+	min: 90,
+	max: 250 
+}; 
+
+const sprite = {
+	enemy: 'images/enemy-bug.png',
+	player: 'images/char-boy.png'
+};
+
+const speed = () => enemySpeed.min + Math.floor(Math.random() * enemySpeed.max);
+const crash = 70;
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+class Enemy {
+	constructor ( x, y, speed, player) {
+		this.x = x;
+		this.y = y;
+		this.speed = speed;
+		this.player = player;
+		this.sprite = sprite.enemy;
+	}	
 
+	checkCollisions () {
+		if(this.player.x + crash > this.x &&
+		   this.player.x < this.x + crash &&
+		   this.player.y + crash > this.y &&
+		   this.player.y < this.y + crash) {
+			this.player.x = playerStart.x;
+			this.player.y = playerStart.y;
+			}
+	}
 
+	update (dt) {		
+		if (this.x > gameField.right) {
+			this.x -= gameField.right;
+			this.speed = speed();
+		}
+		this.x += this.speed * dt;
+		this.checkCollisions();
+	}
+
+	render () {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
+
+class Player {
+	constructor() {
+		this.x = playerStart.x;
+		this.y = playerStart.y;
+		this.sprite = sprite.player;
+	}
+
+	update () {			
+		}
+	
+
+	handleInput (keyPress) {
+		if (keyPress == 'left' && this.x > gameField.left + cell.width) {
+			this.x -= cell.width;
+		}
+		if (keyPress == 'right' && this.x < gameField.right - cell.width) {
+			this.x += cell.width;
+		}
+		if (keyPress == 'up' && this.y > gameField.top) {
+			this.y -= cell.height;
+		}
+		if (keyPress == 'down' && this.y < gameField.bottom - cell.height) {
+			this.y += cell.height;
+		}	
+		else if (this.y  < gameField.top) {			
+			this.x = playerStart.x;
+			this.y = playerStart.y;	
+			alert('You win!');	
+		}		
+	}
+
+	render () {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
+
+const player = new Player(playerStart.x, playerStart.y);
+
+const allEnemies = [63, 147, 230].map(value => {return new Enemy( 0, value, 222, player);}); //need to change
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
